870ddbc9076a153e9124353043a40b6bc7b797c6
lilvinz <lilvinz@nscit.com>
2014-01-15 23:06:10 +0100

    testhal/STM32F4xx/UART: Changed test to show receive issue.

--- testhal/STM32F4xx/UART/main.c
+++ testhal/STM32F4xx/UART/main.c
@@ -17,21 +17,15 @@
 #include "ch.h"
 #include "hal.h"
 
-static VirtualTimer vt3, vt4, vt5;
+static VirtualTimer vt3, vt5;
 
 static const uint8_t message[] = "0123456789ABCDEF";
-static uint8_t buffer[16];
+static uint8_t buffer[1];
 
 static void led3off(void *p) {
 
   (void)p;
   palClearPad(GPIOD, GPIOD_LED3);
-}
-
-static void led4off(void *p) {
-
-  (void)p;
-  palClearPad(GPIOD, GPIOD_LED4);
 }
 
 static void led5off(void *p) {
@@ -55,11 +49,17 @@ static void txend1(UARTDriver *uartp) {
 static void txend2(UARTDriver *uartp) {
 
   (void)uartp;
-  palSetPad(GPIOD, GPIOD_LED5);
   chSysLockFromIsr();
+
+  /* Restart receive. */
+  uartStartReceiveI(&UARTD2, sizeof(buffer), buffer);
+
+  /* Flash led. */
+  palSetPad(GPIOD, GPIOD_LED5);
   if (chVTIsArmedI(&vt5))
     chVTResetI(&vt5);
   chVTSetI(&vt5, MS2ST(200), led5off, NULL);
+
   chSysUnlockFromIsr();
 }
 
@@ -74,35 +74,22 @@ static void rxerr(UARTDriver *uartp, uartflags_t e) {
 }
 
 /*
- * This callback is invoked when a character is received but the application
- * was not ready to receive it, the character is passed as parameter.
- */
-static void rxchar(UARTDriver *uartp, uint16_t c) {
-
-  (void)uartp;
-  (void)c;
-  /* Flashing the LED each time a character is received.*/
-  palSetPad(GPIOD, GPIOD_LED4);
-  chSysLockFromIsr();
-  if (chVTIsArmedI(&vt4))
-    chVTResetI(&vt4);
-  chVTSetI(&vt4, MS2ST(200), led4off, NULL);
-  chSysUnlockFromIsr();
-}
-
-/*
  * This callback is invoked when a receive buffer has been completely written.
  */
 static void rxend(UARTDriver *uartp) {
 
   (void)uartp;
+  chSysLockFromIsr();
+
+  /* Restart receive. */
+  uartStartReceiveI(&UARTD2, sizeof(buffer), buffer);
 
   /* Flashing the LED each time a character is received.*/
   palSetPad(GPIOD, GPIOD_LED3);
-  chSysLockFromIsr();
   if (chVTIsArmedI(&vt3))
     chVTResetI(&vt3);
   chVTSetI(&vt3, MS2ST(200), led3off, NULL);
+
   chSysUnlockFromIsr();
 }
 
@@ -113,7 +100,7 @@ static UARTConfig uart_cfg_1 = {
   txend1,
   txend2,
   rxend,
-  rxchar,
+  NULL,
   rxerr,
   38400,
   0,
@@ -143,7 +130,6 @@ int main(void) {
   palSetPadMode(GPIOA, 2, PAL_MODE_ALTERNATE(7));
   palSetPadMode(GPIOA, 3, PAL_MODE_ALTERNATE(7));
 
-
   /*
    * Normal main() thread activity, in this demo it does nothing.
    */
@@ -153,10 +139,12 @@ int main(void) {
        * Starts both a transmission and a receive operations, both will be
        * handled entirely in background.
        */
-      uartStopReceive(&UARTD2);
-      uartStopSend(&UARTD2);
-      uartStartReceive(&UARTD2, 16, buffer);
-      uartStartSend(&UARTD2, 16, message);
+      chSysLock();
+      if (UARTD2.txstate == UART_TX_IDLE) {
+        uartStopReceiveI(&UARTD2);
+        uartStartSendI(&UARTD2, sizeof(message), message);
+      }
+      chSysUnlock();
     }
     chThdSleepMilliseconds(500);
   }
